62f48f0bf029c9039e13f8a6f3305a9de2a36ff0
stoneneedle <stoneneedle@gmail.com>
2022-04-11 16:13:13 -0400

    ChangedTasksDisplay from lists to Bootstrap cards.

--- src/TasksDisplay.js
+++ src/TasksDisplay.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // Test change
 // Test change 2
+// <li key={index.toString()}>{item}</li>
 
 export default class TasksDisplay extends React.Component {
   constructor(props) {
@@ -10,13 +11,17 @@ export default class TasksDisplay extends React.Component {
 
   render() {
     const tdlItems = this.props.toDoList.map((item, index) => (
-      <li key={index.toString()}>{item}</li>
+      <div className="card text-white bg-secondary mb-3">
+        <div className="card-body">
+          <p className="card-text">{item}</p>
+        </div>
+      </div>
     ));
 
     return (
       <div>
         <p>Tasks Display.</p>
-        <ul>{tdlItems}</ul>
+        {tdlItems}
       </div>
     );
   }
