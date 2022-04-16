880bf823d5979c56ff062e74f36659e53738beef
stoneneedle <stoneneedle@gmail.com>
2022-04-16 15:14:02 -0400

    Removed import statements in App and module depdencies for popperjs, bootstrap, and jQuery, as all moduels use react-bootstrap now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap";
 import "./styles.css";
 import Header from "./components/Header";
 import ShowTask from "./components/ShowTask";
