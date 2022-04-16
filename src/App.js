82a9f8b1978ab3818a17b59e7eb3e9987db3c256
stoneneedle <stoneneedle@gmail.com>
2022-04-16 01:10:26 -0400

    Added react-bootstrap to project & added customized Header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 import "./styles.css";
+import Header from "./components/Header";
 import ShowTask from "./components/ShowTask";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container-fluid">
-        <div className="p-3">
-          <h1>React-TDL</h1>
-          <h2>Project 3</h2>
-          <p className="tdl-item-text">
-            Make your to-do list by adding tasks below.
-          </p>
+    <>
+      <Header />
+      <div className="App">
+        <div className="container-fluid">
+          <div className="p-3">
+            <h1>React-TDL</h1>
+            <h2>Project 3</h2>
+            <p className="tdl-item-text">
+              Make your to-do list by adding tasks below.
+            </p>
+          </div>
+          <ShowTask />
         </div>
-        <ShowTask />
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-TDL</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="https://stoneneedle.github.io/">Home</Nav.Link>
+                <Nav.Link href="https://stoneneedle.github.io/about.html">
+                  About
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
